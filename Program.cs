0038f06b4b4374868f17764b938b6fd24737ff8e
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2021-12-08 22:47:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int [7] {1,3,29,65,5,11,0};
+            float S = 0;
+            float count = S/7;   
+            for (int i = 0; i < 7; i++)
+            { 
+              S+= array[i];
+             count= S/7;    
+                Console.Write ("{0}", array [i]);
+             }
+            Console.WriteLine();
+            Console.WriteLine(S);
+            Console.WriteLine(count);
+            Console.ReadKey();
+
+        }
+    }
+}
